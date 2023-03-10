a99227d4c0cbb53581594c3961e3b31d86ff52ff
terapyo1304 <aryaman020@gmail.com>
2023-03-10 16:11:40 +0000

    requirements added

--- sen_pip.py
+++ sen_pip.py
@@ -1,81 +1,74 @@
-
-from aioitertools import product
-import dotenv
 from sentinelsat import SentinelAPI
-import scipy.misc as misc #not working
-import scipy.ndimage
-from datetime import date
-import imageio as iio
-import geopandas 
-import numpy as np
-import matplotlib.image as mpimg
-from PIL import Image
 from osgeo import gdal
-import pandas as pd
 import xarray as xr
-import re
 import s3fs
 from dotenv import load_dotenv
 import os
-import logging
-import boto3
-from botocore.exceptions import ClientError
 from glob import glob
 import argparse
+import zipfile
 
-from yaml import parse
-parser=argparse.ArgumentParser(description='uploads zarr to s3')
+
+parser = argparse.ArgumentParser(description='sentinel ingestion pipeline')
+parser.add_argument('--product_id', metavar='product_id',
+                    type=str, help='enter product id', required=True)
+args = parser.parse_args()
+product_id = args.product_id
 
 load_dotenv()
-apiusr=os.getenv('apiusr')
-apipass=os.getenv('apipass')
-acc_key=os.getenv('acc_key')
-sec_key=os.getenv('sec_key')
-bucket=os.getenv('bucket')
-api=SentinelAPI(apiusr,apipass)
-s_date=date(2023, 1, 11)
-f_date=date.today()
+# Sentinel Hub credentials
+apiusr = os.getenv('apiusr')
+apipass = os.getenv('apipass')
+api = SentinelAPI(apiusr, apipass)
+
+# s3 credentials
+acc_key = os.getenv('acc_key')
+sec_key = os.getenv('sec_key')
+bucket = os.getenv('bucket')
+endpoint_url = os.getenv('endpoint_url')
+s3 = s3fs.S3FileSystem(
+    key=acc_key,
+    secret=sec_key,
+    client_kwargs={
+        'endpoint_url': endpoint_url
+    }
+)
+
+# downloading images
 query_kwargs = {
-        'platformname': 'Sentinel-2'
-               }
-parser.add_argument('product_id',metavar='product_id',type=str,help='enter product id')
-args=parser.parse_args()
-product_id=args.product_id
-kw=query_kwargs.copy()
-kw['raw']=f'filename:{product_id}*'
-pp=api.query(**kw)
-print(pp)
+    'platformname': 'Sentinel-2'
+}
+
+query_kwargs['raw'] = f'filename:{product_id}*'
+pp = api.query(**query_kwargs)
+# download product
 api.download_all(pp)
+# extract zip file to SAFE folder
+with zipfile.ZipFile(f'{product_id}.zip', 'r') as zip_ref:
+    zip_ref.extractall()
 
+# resampling all the 20m bands
+bands = []
+bands_10 = ['B02', 'B03', 'B04', 'B08']
+bands_20 = ['B05', 'B06', 'B07', 'B11', 'B12']
 
-#resampling all the 20m bands
-bands=[]
-bands_10=['B02','B03','B04','B08']
-bands_20=['B05','B06','B07','B11','B12']
 for item in bands_20:
-        im_path=glob(f'{product_id}.SAFE/GRANULE/*/IMG_DATA/*{item}.jp2')[0]
-        warped_path=im_path.replace(item,f'{item}_warped')
-        ba2=gdal.Warp(warped_path,im_path,xRes=10,yRes=10)
-        bands.append(xr.open_dataarray(warped_path).rename(item))
-        
+    im_path = glob(f'{product_id}.SAFE/GRANULE/*/IMG_DATA/*{item}.jp2')[0]
+    warped_path = im_path.replace(item, f'{item}_warped')
+    ba2 = gdal.Warp(warped_path, im_path, xRes=10, yRes=10)
+    bands.append(xr.open_dataarray(warped_path).rename(item))
+    print(f'{item} resampled and appended')
 
 for item in bands_10:
-        im_path=glob(f'{product_id}.SAFE/GRANULE/*/IMG_DATA/*{item}.jp2')[0]
-        bands.append(xr.open_dataarray(im_path).rename(item))
+    im_path = glob(f'{product_id}.SAFE/GRANULE/*/IMG_DATA/*{item}.jp2')[0]
+    bands.append(xr.open_dataarray(im_path).rename(item))
+    print(f'{item} appended')
 
-bands=xr.merge(bands)
-bands.to_zarr(f'{product_id}.zarr',mode='w')
+# create single zarr
+bands = xr.merge(bands)
+bands.to_zarr(f'{product_id}.zarr', mode='w')
+print('zarr file created')
 
-s3 = s3fs.S3FileSystem(
-      key=acc_key,
-      secret=sec_key,
-      client_kwargs={
-         'endpoint_url': 'http://cyclops.ap-south-1.linodeobjects.com/'
-      }
-   )
-
-s3.put(f'{product_id}.zarr', f's3://cyclops/{product_id}.zarr' , recursive=True)
-        
-
-                     
-
+s3.put(f'{product_id}.zarr',
+       f's3://{bucket}/{product_id}.zarr', recursive=True)
+print('uploaded to s3')
